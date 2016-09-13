20335c242cbeaebb6ce8622a7f3d38490f599b20
n1kk018 <nicolastorero@gmail.com>
2016-09-13 20:51:38 +0200

    dao

---
+++ src/main/java/fr/afcepf/atod/wine/data/admin/impl/DaoAdress.java
@@ -0,0 +1,14 @@
+package fr.afcepf.atod.wine.data.admin.impl;
+
+import java.util.List;
+
+import fr.afcepf.atod.vin.data.exception.WineException;
+import fr.afcepf.atod.wine.data.admin.api.IDaoAdmin;
+import fr.afcepf.atod.wine.data.admin.api.IDaoAdress;
+import fr.afcepf.atod.wine.data.impl.DaoGeneric;
+import fr.afcepf.atod.wine.entity.Admin;
+import fr.afcepf.atod.wine.entity.Adress;
+
+public class DaoAdress extends DaoGeneric<Adress, Integer> implements IDaoAdress {
+
+}
---
+++ src/main/java/fr/afcepf/atod/wine/data/admin/impl/DaoSpecialEvent.java
@@ -0,0 +1,9 @@
+package fr.afcepf.atod.wine.data.admin.impl;
+
+import fr.afcepf.atod.wine.data.admin.api.IDaoSpecialEvent;
+import fr.afcepf.atod.wine.data.impl.DaoGeneric;
+import fr.afcepf.atod.wine.entity.SpecialEvent;
+
+public class DaoSpecialEvent extends DaoGeneric<SpecialEvent, Integer> implements IDaoSpecialEvent {
+
+}
