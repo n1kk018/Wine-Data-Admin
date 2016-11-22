7d73a6f73e0871011107821b351f81dd7370c017
Nicolas <nicolastorero@gmail.com>
2016-11-22 02:59:55 +0100

    Authentification Rest service

--- src/main/java/fr/afcepf/atod/wine/data/admin/api/IDaoAdmin.java
+++ src/main/java/fr/afcepf/atod/wine/data/admin/api/IDaoAdmin.java
@@ -1,8 +1,12 @@
 package fr.afcepf.atod.wine.data.admin.api;
 
+import fr.afcepf.atod.vin.data.exception.WineException;
 import fr.afcepf.atod.wine.data.api.IDaoGeneric;
 import fr.afcepf.atod.wine.entity.Admin;
+import fr.afcepf.atod.wine.entity.Customer;
+import fr.afcepf.atod.wine.entity.User;
 
-public interface IDaoAdmin extends IDaoGeneric<Admin, Integer> {
-	
+public interface IDaoAdmin extends IDaoGeneric<User, Integer> {
+	User connect(String mail, String password) throws WineException;
+	Admin addNewAdmin(Admin admin);
 }
--- src/main/java/fr/afcepf/atod/wine/data/admin/impl/DaoAdmin.java
+++ src/main/java/fr/afcepf/atod/wine/data/admin/impl/DaoAdmin.java
@@ -1,18 +1,61 @@
 package fr.afcepf.atod.wine.data.admin.impl;
 
-import java.util.List;
-
 import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Service;
 
+import fr.afcepf.atod.vin.data.exception.WineErrorCode;
 import fr.afcepf.atod.vin.data.exception.WineException;
 import fr.afcepf.atod.wine.data.admin.api.IDaoAdmin;
 import fr.afcepf.atod.wine.data.impl.DaoGeneric;
 import fr.afcepf.atod.wine.entity.Admin;
+import fr.afcepf.atod.wine.entity.User;
 
 @Service
 @Transactional
-public class DaoAdmin extends DaoGeneric<Admin, Integer> implements IDaoAdmin {
+public class DaoAdmin extends DaoGeneric<User, Integer> implements IDaoAdmin {
+    /*****************************************************.
+     *                  Requetes HQL
+     ****************************************************/
+
+    private static final String REQCONNEXION = "SELECT u FROM User u "
+                + "WHERE  u.mail     = :paramMail "
+                + "AND   u.password = :paramPassword "
+                + "AND u.user_type = 'ADMIN'";
+
+    /****************************************************.
+     *                 Fin Requetes HQL
+     ****************************************************/
+    @Override
+    public User connect(String paramMail, String paramPassword) throws WineException {
+        User user = null;
+        if (!paramMail.equalsIgnoreCase("") && 
+                !paramPassword.equalsIgnoreCase("")) {
+           user = (User) (getSf().getCurrentSession()
+                   .createQuery(REQCONNEXION)
+                   .setParameter("paramMail", paramMail)
+                   .setParameter("paramPassword", paramPassword)
+                   .uniqueResult());
+ 
+            if (!user.getLastname().equalsIgnoreCase("")){
+                return user;
+            } else {
+                throw  new WineException(WineErrorCode.LOGIN_MDP_INVALIDE, 
+                    "user not in the db");
+            }
+        } else {
+            throw  new WineException(WineErrorCode.LOGIN_MDP_INVALIDE, 
+                    "mail  or password invalid");
+        }
+    }
+
+    @Override
+    public Admin addNewAdmin(Admin paramAdmin) {
+        Admin admin = null;
+        admin = (Admin) getSf().getCurrentSession().save(paramAdmin);
+        return admin;
+    }
+
+    
 
 }
